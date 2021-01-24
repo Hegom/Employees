de976fb4c92447f89cbc9972d36f951ad1e6fe87
Hector Gomez <hector.gomez@energage.com>
2021-01-24 17:32:34 -0500

    UI Implementation, JS and Bootstrap

--- Employees.Web/wwwroot/js/site.js
+++ Employees.Web/wwwroot/js/site.js
@@ -1,4 +1,23 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.getElementById("getEmployeesButton").addEventListener("click", function (event) {
+    var employeeId = document.getElementById("employeeInput").value;
 
-// Write your Javascript code.
+    fetch(`http://localhost:50266/api/Employee/${employeeId}`)
+        .then((response) => {           
+            if (response.status == 400) {
+                alert(`Error: ${response.statusText}`)
+            }
+            if (response.status != 204) {
+                return response.json();
+            }
+            return [];
+        })
+        .then((data) => {
+            console.log(data);
+            $('#employeesTable').bootstrapTable({
+                data: data
+            });
+            // Refresh
+            $('#employeesTable').bootstrapTable('load', data);
+        })
+        .catch((error) => console.log(error))
+});
